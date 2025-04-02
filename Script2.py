4a842a69bb09c31f042511637ad7e47ede9a9f6a
HugoPires42 <127425190+HugoPires42@users.noreply.github.com>
2025-04-03 00:30:30 +0200

    Create Script2.py

---
+++ Script2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Charger le fichier texte
+file_path = "C:\Users\hugop\Downloads\PS_LibreAcces_202504020841\PS_LibreAcces_SavoirFaire_202504020841.txt"  # Remplacez par le chemin de votre fichier
+df = pd.read_csv(file_path, sep='|', encoding='utf-8')
+
+# Exporter le DataFrame en fichier Excel
+excel_file_path = 'sortie.xlsx'  # Remplacez par le chemin de sortie souhaité
+df.to_excel(excel_file_path, index=False)
+
+print(f"Le fichier Excel a été créé : {excel_file_path}")
